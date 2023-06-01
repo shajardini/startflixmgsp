b0882fd6cf952015f89afdd51c4814a53e9bc446
shajardini <sha.jardini@gmail.com>
2023-06-01 14:59:26 -0300

    atualização

--- src/routes.js
+++ src/routes.js
@@ -1,11 +1,12 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/home";
 import Filme from "./pages/filme";
+import Header from "./components/header";
 
 function RotasApp(){
     return(
        <BrowserRouter>
-
+            <Header/>
             <Routes>
                 <Route path='/' element={<Home/>}/>
                 <Route path='/filme/:id' element={<Filme/>}/>
