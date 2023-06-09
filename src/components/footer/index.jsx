34f0d71361331d2438f9cdd7dddcf88e0107e8a9
shajardini <sha.jardini@gmail.com>
2023-06-09 18:10:51 -0300

    Atualização

--- src/components/footer/index.jsx
+++ src/components/footer/index.jsx
@@ -1,6 +1,8 @@
+import './footer.css'
+
 export default function Footer(){
     return(
-        <footer>
+        <footer className='footer-fixo'>
             <p>Shay</p>
         </footer>
     )
