4c86b62c276d8108dcdb25d92a7f1ccb8029ded4
shajardini <sha.jardini@gmail.com>
2023-06-09 15:08:48 -0300

    Atualização

--- src/pages/filme/index.jsx
+++ src/pages/filme/index.jsx
@@ -1,4 +1,29 @@
+import './filme.css'
+import {useState, useEffect} from 'react'
+import api from '../../services/api'
+import { useParams } from 'react-router-dom'
+
+
 function Filme(){
+
+const {id} = useParams()
+
+const [filme, setFilme] = useState([])
+
+
+useEffect(()=>{
+  async function carregaFilme(){
+    const response = await api.get(`/${id}`)
+    setFilme(response)
+
+  }
+  carregaFilme()
+},[])
+
+
+
+
+
     return(
       <div>
           <h1>Bem vindo a Filme!</h1>
