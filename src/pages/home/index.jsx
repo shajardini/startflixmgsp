154ee7405b81fa48859864b9e56884e43a09da6a
shajardini <sha.jardini@gmail.com>
2023-06-09 11:15:19 -0300

    atualização

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -1,10 +1,41 @@
- function Home(){
-  return(
+import api from "../../services/api"
+import { useState, useEffect } from 'react'
+import { Link } from "react-router-dom"
+
+
+
+function Home() {
+  const [filmes, setFilmes] = useState([])
+
+
+  useEffect(() => {
+    async function carregarFilme() {
+      const resposta = await api.get()
+      console.log(resposta.data)
+      setFilmes(resposta.data)
+    }
+
+
+    carregarFilme()
+  }, [])
+
+
+  return (
     <div>
-        <h1>Bem vindo a Home!</h1>
+      <div className="lista">
+        {filmes.map((filme) => {
+          return (
+            <article key={filme.id}>
+              <strong>{filme.titulo}</strong>
+              <img src={filme.url_img} alt={filme.titulo}/>
+              
+            </article>
+          )
+        })}
+      </div>
     </div>
-  )  
+  )
 }
 
-export default Home
 
+export default Home
